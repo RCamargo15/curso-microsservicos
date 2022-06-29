9a214ef4224ee192ab64db2b93eef1180902d1a9
RCamargo15 <rafael.camargo384@gmail.com>
2022-06-29 01:03:14 -0300

    RestTemplate

--- hr-payroll/src/main/java/com/rcamargo15/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/rcamargo15/hrpayroll/services/PaymentService.java
@@ -1,12 +1,38 @@
 package com.rcamargo15.hrpayroll.services;
 
-import com.rcamargo15.hrpayroll.entities.Payment;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.rcamargo15.hrpayroll.entities.Payment;
+import com.rcamargo15.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
 
+	@Value("${hr-worker.host}")
+	private String HR_WORKER;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
     public Payment getPayment(long workerId, int days){
-        return new Payment("Bob", 200.0, days);
+    	
+    	Map<String, String> uriVariable = new HashMap<>();
+    	uriVariable.put("id", String.valueOf(workerId));
+    	
+    	HttpEntity<Object> entity = new HttpEntity<>(uriVariable); 
+    	
+    	ResponseEntity<Worker> worker = restTemplate.exchange(HR_WORKER.concat(String.valueOf(workerId)), HttpMethod.GET, entity, Worker.class, uriVariable);
+    	Worker w1 = worker.getBody();
+    	
+        return new Payment(w1.getName(), w1.getDailyIncome(), days);
     }
 }
