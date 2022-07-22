9303156afc81fba6b974fff70ae5a8baea6366c1
RCamargo15 <rafael.camargo384@gmail.com>
2022-07-22 12:05:10 -0300

    UserRepository, RoleRepository, userResource, gateway config

--- hr-user/src/main/java/com/RCamargo15/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/RCamargo15/hruser/HrUserApplication.java
@@ -1,15 +1,45 @@
 package com.RCamargo15.hruser;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import com.RCamargo15.hruser.entities.Role;
+import com.RCamargo15.hruser.entities.User;
+import com.RCamargo15.hruser.repositories.RoleRepository;
+import com.RCamargo15.hruser.repositories.UserRepository;
+
 @EnableEurekaClient
 @SpringBootApplication
-public class HrUserApplication {
+public class HrUserApplication implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	@Autowired
+	private RoleRepository repo1;
 
 	public static void main(String[] args) {
-		SpringApplication.run(HrUserApplication.class, args);
+		SpringApplication.run(HrUserApplication.class);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		User u1 = new User(null, "Rafael", "rafael.camargo384@gmail.com", "j9A0I7!#z8&o9j@2T&uYn9*3@WQFn7");
+		User u2 = new User(null, "Edilaine", "edilaineoliveira@gmail.com", "4u%mnFSJwP5u4jd6BZF3Z6&IGj7WSK");
+		repo.saveAll(Arrays.asList(u1,u2));
+		Role r1 = new Role(null, "ROLE_OPERATOR");
+		Role r2 = new Role(null, "ROLE_ADMIN");
+		repo1.saveAll(Arrays.asList(r1,r2));
+		
+		u1.getRoles().add(r1);
+		u1.getRoles().add(r2);
+		u2.getRoles().add(r1);
+		repo.saveAll(Arrays.asList(u1,u2));
+		
+	}
 }
---
+++ hr-user/src/main/java/com/RCamargo15/hruser/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.RCamargo15.hruser.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.RCamargo15.hruser.entities.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long>{
+
+}
